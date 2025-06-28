e98b4ba6d2db2985fd240a11a9f310588df3d8c3
The-Ossuary <the.ossuary.xyz@gmail.com>
2025-06-28 10:25:57 -0700

    Update tailwind.config.js

--- contracts/ossuary-frontend/ossuary-frontend/tailwind.config.js
+++ contracts/ossuary-frontend/ossuary-frontend/tailwind.config.js
@@ -1,1 +1,17 @@
-kjk
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "osien-dark": "#1b1b1b",
+        "bone-white": "#f0f0e6",
+        "crypt-gray": "#444",
+      },
+      fontFamily: {
+        serif: ["Times New Roman", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
